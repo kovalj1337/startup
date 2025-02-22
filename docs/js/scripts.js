e95e70296cee321320659fcfd3cfc6b3180fed58
kovalj1337 <vanaplayers@gmail.com>
2025-02-22 12:24:38 +0200

    dds

---
+++ docs/js/scripts.js
@@ -0,0 +1,1 @@
+const burger=document.querySelector(".burger"),bgcMobile=document.querySelector(".bgc-blur-for-mobile"),menu=document.querySelector(".menu-panel"),cancel=document.querySelector(".cancel");burger.addEventListener("click",(function(){bgcMobile.style.right="0",menu.style.left="0"})),cancel.addEventListener("click",(function(){bgcMobile.style.right="-100%",menu.style.left="-100%"}));
